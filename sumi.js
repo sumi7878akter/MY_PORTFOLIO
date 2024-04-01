635d4437c5a2855b81b532388d40883c77dd6432
sumi7878akter <jannatulislamsumi0@gmail.com>
2024-04-01 12:27:25 +0600

    form

--- sumi.js
+++ sumi.js
@@ -9,3 +9,28 @@ const project = {firstName:"john", lastName:"doe", age :45, eyecolor:"blue"};
 console.log(project)
 
 }
+{
+    const form = document.querySelector('form');
+const yourName = document.querySelector('#yourname');
+const email = document.querySelector('#email');
+const textarea = document.querySelector('#text');
+// console.log(form)
+
+form.addEventListener('submit', formhandler);
+
+function formhandler(e){
+    e.preventDefault();
+
+    const formInfo = {
+        yourName:yourName.value,
+        email:email.value,
+        textarea:textarea.value
+    }
+    console.log(formInfo);
+
+    yourName.value = '',
+    email.value = '',
+    textarea.value = ''
+
+}
+}
